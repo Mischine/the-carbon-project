3bc09e1e5bc30ec81f0e976746f45e5ffc2512b5
Mischa <mischinery@gmail.com>
2014-02-24 02:10:06 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	plugins/carbon.lua

--- plugins/carbon_reload.lua
+++ plugins/carbon_reload.lua
@@ -8,6 +8,7 @@ PLUGIN.Author = 'Mischa & CareX'
 --||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 function PLUGIN:Init()
     self:AddChatCommand('reload', self.cmdReload)
+    self:AddChatCommand('reloader', self.cmdReloader)
 end
 
 --||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -32,6 +33,31 @@ function PLUGIN:cmdReload( netuser )
     if not reloadtoken then
         local b, str = reloadCarbon('carbon')
         rust.Notice(netuser, str)
+        rust.RunServerCommand( 'wildlife.forceupdate' )
+    end
+end
+function reloaderCarbon(carbon)
+    reloadtoken = timer.Once(3,function() reloadtoken = nil  end)
+    print('Carbon reloader initiated.. .')
+    cs.reloadplugin(carbon)
+    local cplugin = plugins.Find(carbon)
+    if cplugin then
+        cplugin:Init()
+        if cplugin.PostInit then cplugin:PostInit() end
+    else
+        return false, 'Failed to reload carbon reloader'
+    end
+    print('Carbon reloader complete.')
+    return true, 'Carbon reloaded'
+end
+local RustNoticeTemplate = { RustFirstPass.NetUser, "string", "number" }
+function PLUGIN:cmdReloader( netuser )
+    if not reloadtoken then
+        local b, str = reloaderCarbon('carbon_reload')
+        local text = 'this is a test'
+        local duration = 2
+        Rust.Rust.Notice.Popup( netuser.networkPlayer, " ", text, duration or 4.0 )
+        --rust.Notice( netuser, "   ", text, duration or 4.0 )
         rust.RunServerCommand( 'wildlife.forceupdate' )
     end
 end
