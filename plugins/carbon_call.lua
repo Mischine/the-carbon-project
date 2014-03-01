223558bfcee0db2a821eec7ac1b9105ba62e0431
Mischa <mischinery@gmail.com>
2014-03-01 06:34:18 -0800

    Minor code clean up -carbon utils separated.

--- plugins/carbon_call.lua
+++ plugins/carbon_call.lua
@@ -1,6 +1,6 @@
 PLUGIN.Title = 'carbon_call'
 PLUGIN.Description = 'guild call module'
-PLUGIN.Version = '0.0.1'
+PLUGIN.Version = '0.0.2'
 PLUGIN.Author = 'mischa / carex'
 
 function PLUGIN:Init()
