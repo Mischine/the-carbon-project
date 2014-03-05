2838656fe7edd6c67860a47585f271f3d1c9d3b0
Mischa <mischinery@gmail.com>
2014-03-04 16:39:31 -0800

    Combat/Char still a work in progress. (so far.. its working.. but! had to be rewritten)

--- plugins/carbon_a.lua
+++ plugins/carbon_a.lua
@@ -2,3 +2,7 @@ PLUGIN.Title = 'carbon_sandbox_a'
 PLUGIN.Description = 'sandbox module'
 PLUGIN.Version = '0.0.1'
 PLUGIN.Author = 'Mischa & CareX'
+
+function PLUGIN:Init()
+    core = cs.findplugin("carbon_core") core:LoadLibrary()
+end
